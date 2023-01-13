4d58e7f602c1cad0c93e0aa06bb0973fb7b4b8c8
PhucDev02 <phuc12610@gmail.com>
2023-01-13 17:07:22 +0700

    update challengeManager in gameManager

--- Assets/Scripts/ScriptableObject/Level/NoAimLevel.cs
+++ Assets/Scripts/ScriptableObject/Level/NoAimLevel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "NoAim", menuName = "Level/No Aim Level")]
 public class NoAimLevel : Level
 {
 }
